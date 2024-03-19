de03ad83d9c07826940991ea0604ae0265ca60a2
haneeshaseef <haneeshaseef521@gmail.com>
2024-03-19 17:08:11 +0530

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+    <img src='/assets/logo.svg'/>
+    <nav>
+        <ul>
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Menu</a></li>
+            <li><a>Reservations</a></li>
+            <li><a>order Online</a></li>
+            <li><a>Login</a></li>
+        </ul>
+    </nav>
+    </>
+  )
+}
+
+export default Nav
